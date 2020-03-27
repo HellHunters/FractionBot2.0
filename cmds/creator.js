0974d861f74bcec73822c24318436927a2e51326
Asor1k <roma.dotsenko@gmail.com>
2020-03-27 16:48:42 +0200

    As

--- cmds/creator.js
+++ cmds/creator.js
@@ -2,6 +2,7 @@ const Discord = module.require("discord.js");
 const fs = require("fs");
 module.exports.run = async (bot , message , args) => {
     message.channel.send('Hellamb is my creator!')
+    
 };
 
 module.exports.help = {
--- cmds/warn.js
+++ cmds/warn.js
@@ -1,12 +1,14 @@
 const Discord = module.require("discord.js");
-let profile = require('./profile.json');
+let profile = require('../profile.json');
 const fs = require("fs");
 module.exports.run = async (bot , message , args) => {
-    let userWarns = profile.warns;
+    let userId = args[0].slice(3);
+    let userWarns = profile[message.guild.id].members[userId].warns;
     
 };
 
 module.exports.help = {
+   name: "warn"
    
     
 }
