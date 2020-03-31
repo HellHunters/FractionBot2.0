72c13ad82224c3d54f3d4c46ebeb31fbd510f117
Hellamb <vlad.kuksiuk2@gmail.com>
2020-03-31 16:38:12 +0300

    wordGame v 0.0.1

--- cmds/wordgame.js
+++ cmds/wordgame.js
@@ -1,66 +1,92 @@
 const Discord = module.require("discord.js");
 const fs = require("fs");
 //let wordGameHandler = require('../wordGameHandler.json');
-let wordGameHandler = require('../wordGameHandler.json');
+let wordGameHandler = require('../wordGameHandler')
 
-module.exports.run = async (bot , message , args) => {
-    let channelid = message.channel.id;
-    
-      wordGameHandler[channelid] ={
-        GameId: message.channel.id,
-        Words: "",
-        LastWord: "А",
-        CurWord: "",
-        LastWordPlayerId: 0 ,
-        PlayerCount: 0,
-        Play: true,
-        Players : {}
-      
-    }
-    for(key of message.channel.members)
-    {
-        let userid = key[0];
-        let user = message.guild.member(userid).user;
-        
-      wordGameHandler[channelid].Players[userid] =
-      {
-        name: user.username,
-        points: 0,
-        lastWord: "",
-      }
-    }; 
-    console.log(message.channel.members);
-    fs.writeFile('../wordGameHandler.json', JSON.stringify(wordGameHandler, null, '\t'), (err)=> //Writting JSON file
+function sleep(millis)
+{
+    var date = new Date();
+    var curDate = null;
+    do { curDate = new Date(); }
+    while(curDate-date < millis);
+}
+
+
+let isPlay = false;
+
+module.exports.run = async (bot , message , args) => 
+{ 
+
+
+
+  wordGameHandler["game"] = 
+  {
+    players : {},
+    channelID:"",
+    usedWords: [],
+    currentLetter:"",
+  }
+
+  fs.writeFile('./wordGameHandler.json', JSON.stringify(wordGameHandler, null, '\t'), (err)=> //Writting JSON file
     {
        if(err) console.log(err);
     });
 
-    message.channel.send("Игра началась!");
-    message.channel.send("Первая буква: A");
-    
-    let lastWord = wordGameHandler[channelid].LastWord;
-    while(wordGameHandler[channelid].Play){
-       
-        let curAr =  wordGameHandler[channelid].CurWord.toLowerCase().split('');
-         
-         let lArr = wordGameHandler[channelid].LastWord.toLowerCase().split('');
-        if(curAr[0]==lArr[lArr.length-1]){
-            console.log(curAr);
-            wordGameHandler[channelid].LastWord = wordGameHandler[channelid].CurWord;
-            wordGameHandler[channelid].CurWord = "";
-            console.log("2 enter");
-        }
-        if(wordGameHandler[channelid].LastWord==lastWord)continue;
-        if(wordGameHandler[channelid].LastWord!=lastWord){
-            lastWord = wordGameHandler[channelid].lastWord;
-            let lastId = wordGameHandler[channelid].LastWordPlayerId;
-            wordGameHandler[channelid].Players[lastId]++;
-            message.channel.send(message.guild.member(lastId)+" заработал 1 очко!");
-        }
-    }
-    
-};
+    let handler = wordGameHandler["game"]
 
+    let msg = await message.channel.send("Чтобы присоедениться поставьте реакцию! У вас есть 30сек");
+    await msg.react('✅');
+    const reactions = await msg.awaitReactions(reaction => {
+      //console.log(reaction.users.cache)
+      for(key of reaction.users.cache){
+        console.log(msg.guild.members)
+        if(msg.guild.member.get(key[0]).bot){
+          console.log("1")
+ 
+        }else
+        {
+           console.log("2")
+          handler.players[reaction.message.author.id] = {
+            name: reaction.message.author.username,
+            hp : 3,
+            score: 0
+ 
+             }
+           fs.writeFile('./wordGameHandler.json', JSON.stringify(wordGameHandler, null, '\t'), (err)=> //Writting JSON file
+           {
+             if(err) console.log(err);
+           });
+        } 
+
+      }
+     
+
+      
+    },{time:30000})
+
+    console.log(msg)
+
+
+
+    while(isPlay)
+    {
+
+
+      const filter = m => m.author.id === message.author.id;
+      message.reply("waiting 10s").then(r => r.delete(10000));
+      await message.channel.awaitMessages(filter,{max: 1, time: 10000}).then(collected =>  
+      {
+
+        var word = collected.first().content;
+
+        message.channel.send(word);
+
+
+      }).catch(err =>{
+      console.log(err);
+      });
+    }
+}
 module.exports.help = {
     name: "wordgame"
 }
